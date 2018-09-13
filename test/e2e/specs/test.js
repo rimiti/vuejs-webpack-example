2533f694c89e89489fb1d4978db58297b9393063
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-09-13 10:31:52 +0200

    chore(test/e2e/specs/test.js) test example created

---
+++ test/e2e/specs/test.js
@@ -0,0 +1,12 @@
+// const nightwatchConfig = require('../nightwatch.conf');
+
+module.exports = {
+  Home: function test() {
+    // browser
+    //   .url(nightwatchConfig.test_settings.default.launch_url)
+    //   .waitForElementVisible('#app', 5000)
+    // .assert.containsText('h1', 'Welcome to Your Vue.js App')
+    // .assert.elementCount('img', 1)
+    // .end();
+  },
+};
